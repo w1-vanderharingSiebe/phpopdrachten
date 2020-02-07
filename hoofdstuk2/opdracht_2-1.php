31bcd1bf1d7baec36c35455d56f093825e499e28
Siebe van der Haring <siebevanderharing@edu-kw1c.nl>
2020-02-07 13:39:53 +0100

    opdracht_2-1.php en template.php toegevoegd

---
+++ hoofdstuk2/opdracht_2-1.php
@@ -0,0 +1,29 @@
+<?php
+/**
+*user: Siebe van der Haring
+*Date: 5-2-2020
+*Time: 13:30
+*File: Index.php
+*/
+?>
+
+<!DOCTYPE.html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="opdracht x">
+        <meta name="keywords" content="keyword 1, keyword 2">
+        <link href="style.css" type="text/css" rel="stylesheet">
+          <title>
+                Template
+          </title>
+    </head>
+        <body>
+            <p>
+               <?php
+                echo "Hello World"
+                ?>
+            </p>
+        </body>
+</html>
